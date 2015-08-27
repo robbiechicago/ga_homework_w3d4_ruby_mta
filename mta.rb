e8c21fa9a56da24755a5a4bdc31a5f78f5783ce5
Rob Holloway <robbiechicago@gmail.com>
2015-08-27 18:34:37 +0100

    putsing a whole load of info as I work out the two-lines logic.

--- mta.rb
+++ mta.rb
@@ -8,40 +8,47 @@ print 'Select your starting line (l, n or 6): '
 start_line = gets.chomp
 
 if start_line == 'l'
-  start_line = :linel
+  start_line_symbol = :linel
 elsif start_line == 'n'
-  start_line = :linen
+  start_line_symbol = :linen
 else
-  start_line = :line6
+  start_line_symbol = :line6
 end
 
-print mta[start_line]
+print mta[start_line_symbol]
 
 puts
 print 'Select your starting station: '
 start = gets.chomp
-puts start
+
 
 print 'Select your ending line (l, n or 6): '
 end_line = gets.chomp
 
 if end_line == 'l'
-  end_line = :linel
+  end_line_symbol = :linel
 elsif end_line == 'n'
-  end_line = :linen
+  end_line_symbol = :linen
 else
-  end_line = :line6
+  end_line_symbol = :line6
 end
 
-print mta[end_line]
+print mta[end_line_symbol]
 puts
 print 'select an end station: '
 the_end = gets.chomp
 
-puts mta[start_line].index(start)
-puts mta[end_line].index(the_end)
+start_index = mta[start_line_symbol].index(start)
+end_index = mta[end_line_symbol].index(the_end)
 
-stops = (mta[start_line].index(start) - mta[end_line].index(the_end)).abs
+puts 'start line = ' + start_line.to_s
+puts 'start station = ' + start.to_s
+puts 'end line = ' + end_line.to_s
+puts 'end station = ' + the_end.to_s
+puts 'start station index = ' + start_index.to_s
+puts 'end station index = ' + end_index.to_s
+
+stops = (mta[start_line_symbol].index(start) - mta[end_line_symbol].index(the_end)).abs
 puts stops
 
 
